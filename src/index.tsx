8aeb7d2cabed1a020d06bf5bc2759a8715abedd6
김우정 <wjkim@rsupport.com>
2021-07-13 23:30:35 +0900

    feat: transpiling using esbuild works well

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,45 @@
+import { useState, useEffect, useRef } from 'react';
 import ReactDOM from 'react-dom';
+import * as esbuild from 'esbuild-wasm';
 
 const App = () => {
-    return <h1>Hello World!</h1>;
+    const ref = useRef<any>();
+    const [input, setInput] = useState("");
+    const [code, setCode] = useState("");
+
+    const startService = async () => {
+        ref.current = await esbuild.startService({
+            worker: true,
+            wasmURL: "/esbuild.wasm"
+        })
+    }; 
+
+    useEffect(() => {
+        startService();
+    }, [])
+
+    const handleClick = async () => {
+        if (!ref.current) {
+            return;
+        }
+
+        const result  = await ref.current.transform(input, {
+            loader: 'jsx',
+            target: 'es2015'
+        });
+
+        setCode(result.code);
+    }
+
+    return (
+        <div>
+            <textarea value={input} onChange={(e) => setInput(e.target.value)}></textarea>
+            <div>
+                <button onClick={handleClick}>Submit</button>
+            </div>
+            <pre>{code}</pre>
+        </div>
+    );
 };
 
 ReactDOM.render(
